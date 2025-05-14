fc8bbbb605d7a24d793c1c5b91aa8d21f065328d
Anugrah Sebastian <87081811+Anugrah-T-Sebastian@users.noreply.github.com>
2025-05-15 01:41:17 +0530

    Annotations added

--- 01-StudentREST/student/student/src/main/java/com/pratice/student/repositories/StudentRepository.java
+++ 01-StudentREST/student/student/src/main/java/com/pratice/student/repositories/StudentRepository.java
@@ -1,8 +1,9 @@
 package com.pratice.student.repositories;
 
 import com.pratice.student.entity.Student;
-import org.hibernate.dialect.MySQLStorageEngine;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 }
