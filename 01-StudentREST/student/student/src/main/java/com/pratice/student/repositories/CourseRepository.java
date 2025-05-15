95e43b76855c0a7004e12da1b42e2c9e441cad64
Anugrah Sebastian <87081811+Anugrah-T-Sebastian@users.noreply.github.com>
2025-05-16 01:22:01 +0530

    JPA derived queries added for StudentRepository and CourseRepository

--- 01-StudentREST/student/student/src/main/java/com/pratice/student/repositories/CourseRepository.java
+++ 01-StudentREST/student/student/src/main/java/com/pratice/student/repositories/CourseRepository.java
@@ -1,9 +1,14 @@
 package com.pratice.student.repositories;
 
 import com.pratice.student.entity.Course;
+import com.pratice.student.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    List<Course> findAllByStudent(Student studentById);
 }
--- 01-StudentREST/student/student/src/main/java/com/pratice/student/repositories/StudentRepository.java
+++ 01-StudentREST/student/student/src/main/java/com/pratice/student/repositories/StudentRepository.java
@@ -1,9 +1,18 @@
 package com.pratice.student.repositories;
 
+import com.pratice.student.entity.Course;
 import com.pratice.student.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    @Query("SELECT s.courses FROM Student s WHERE s.id = :studentId")
+    List<Course> findCourseById(@Param("studentId") Long studentId);
+
+    Student findStudentById(Long studentId);
 }
